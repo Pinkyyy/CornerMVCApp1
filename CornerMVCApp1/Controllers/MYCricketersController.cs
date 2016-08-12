dee6d049483ee1f4a5e375eef4201d10ab16c41a
Pinkyyy <pinkyyy@vp.pl>
2016-08-12 12:47:27 +0200

    Adding my new project

---
+++ CornerMVCApp1/Controllers/MYCricketersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CornerMVCApp1.Controllers
+{
+    public class MYCricketersController : Controller
+    {
+        // GET: MYCricketers
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
